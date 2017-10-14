ebba744a53f277010180eec6d8498b33a40ebbd8
reelardan <30440219+reelardan@users.noreply.github.com>
2017-10-14 21:12:24 +0700

    Add files via upload

---
+++ view.php
@@ -0,0 +1,27 @@
+<html>
+<body align="center">
+<h3>Hasil Input</h3>
+<table align="center">
+<tr>
+<td>E-Mail<td>:</td></td>
+<td> <?php echo $_POST['email']; ?></td>
+</tr>
+<tr>
+<td>Fullname<td>:</td></td>
+<td> <?php echo $_POST['fullname']; ?></td>
+</tr>
+<tr>
+<td>Username<td>:</td></td>
+<td> <?php echo $_POST['username']; ?></td>
+</tr>
+<tr>
+<td>Password<td>:</td></td>
+<td> 
+<?php 
+echo $_POST['password']; 
+?>
+</td>
+</tr>
+</table>
+</body>
+</html>
